85d000bbbb283779d9818c62558c7226ca54aecf
Zannatun Naim Sristy <zannatunsristy@gmail.com>
2020-10-01 01:15:59 +0600

    HomeScreen Component added

--- Apps/app01/src/screen/HomeScreen.js
+++ Apps/app01/src/screen/HomeScreen.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image,TouchableOpacity,Button} from 'react-native';
 
 const HomeScreen = () => {
-    return(<Text style={styles.textStyle}>Home</Text>);
+    return(
+        <View>
+            <Image style={styles.imageStyle} source={require('./../../assets/Iut.png')}/>
+            <Text style={styles.textStyle}>Department of CSE</Text>
+            <Text style={styles.textStyle}>Programme: SWE</Text>
+            <TouchableOpacity style={styles.touchableStyle}>
+            <Text style={styles.touchabletextStyle}>My Profile</Text>            
+            </TouchableOpacity>
+            <Button
+            title='Semesterwise Course List'
+            color='skyblue'
+            />
+            <Button
+            title='List of Faculty Member'
+            color='steelblue'
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create(
     {
-        textStyle:{
+       textStyle:{
             fontSize: 20,
-            color: 'red',
+            color: 'black',
             alignSelf: 'center',
-        }
+            fontWeight: 'bold',
+        },
+
+        imageStyle:{
+            height: 170,
+            width: 100,
+            alignSelf: 'center',
+            marginTop: 100,
+            marginBottom: 30,
+        },
+
+        touchableStyle:{ 
+            marginTop:100,
+            marginBottom: 40,
+            height: 40,           
+            width: 200,
+            backgroundColor: 'yellow',
+            alignSelf: 'center',
+        },
+        touchabletextStyle:{
+            marginTop: 5,
+            fontSize: 20,
+            color: 'black',
+            alignSelf: 'center',
+        },
     }
 )
 
